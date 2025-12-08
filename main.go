b405c939f56a7c734aa6987272c3d22679d245b0
nichigovsky <inichigovsky@gmail.com>
2025-12-08 22:18:25 +0300

    init

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import "time"
+
+type Bin struct {
+	id string
+	private bool
+	createdAt time.Time
+	name string
+}
+
+type BinList = []Bin
+
+func createList() {
+	sli := make([]BinList, 2)
+}
+
 func main() {
-	
+
 }
