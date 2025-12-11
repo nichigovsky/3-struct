408be4642b1d6759e1d2ef4e13282708e4b2b070
nichigovsky <inichigovsky@gmail.com>
2025-12-11 23:41:10 +0300

    feat: add functions

--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func ReadFile(name string) ([]byte, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, errors.New("COULD_NOT_READ_FILE")
+	}
+
+	return data, nil
+}
+
+func WriteFile(content []byte, name string) error {
+	file, err := os.Create(name)
+
+	if err != nil {
+		return errors.New("COULD_NOT_CREATE_FILE")
+	}
+
+	_, err = file.Write(content)
+	defer file.Close()
+	
+	if err != nil {
+		return errors.New("COULD_NOT_WRITE_FILE")
+	}
+
+	fmt.Println("File has been created")
+
+	return nil
+}
